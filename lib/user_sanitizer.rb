8090e0b6481b3518258c98fd3d594c6e4ef58aad
Andrew Ng <andrewkawing@gmail.com>
2014-03-11 13:49:38 -0700

    fix profile pages

--- lib/user_sanitizer.rb
+++ lib/user_sanitizer.rb
@@ -1,7 +1,7 @@
 class User::ParameterSanitizer < Devise::ParameterSanitizer
     private
     def account_update
-        default_params.permit(:first_name, :last_name, :description, :profile_image, :email, :password, :facebook, :twitter, :instagram, :linkedin, :pinterest, :password_confirmation, :current_password)
+        default_params.permit(:first_name, :last_name, :description, :profile_image, :email, :password, :facebook, :twitter, :instagram, :linkedin, :pinterest, :password_confirmation, :current_password, :about, :work, :education)
     end
     def sign_in
         default_params.permit(:email, :password)
